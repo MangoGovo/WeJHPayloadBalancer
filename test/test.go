141bcb26fe65935f0135fd820ac27249ce20882a
MangoGovo <1749100231@qq.com>
2024-10-20 15:15:03 +0800

    feat:实现了测试接口

---
+++ test/test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/api/test", test)
+	_ = r.Run()
+}
+
+type requestData struct {
+	MaxDelay    int     `form:"max_delay" bind:"gt=0"`
+	FailureRate float64 `form:"failure_rate" bind:"gt=0"`
+}
+
+func test(c *gin.Context) {
+	data := requestData{
+		MaxDelay:    0,
+		FailureRate: 0,
+	}
+	err := c.ShouldBind(&data)
+
+	if err != nil {
+		c.JSON(500, gin.H{"msg": "params error"})
+		return
+	}
+
+	// 概率失败
+	if rand.Float64() < data.FailureRate {
+		c.JSON(500, gin.H{"msg": "error"})
+		return
+	}
+
+	// 随机延时
+	delay := 0
+	if data.MaxDelay > 0 {
+		delay = rand.Intn(data.MaxDelay)
+		time.Sleep(time.Duration(delay) * time.Millisecond)
+	}
+
+	c.JSON(200, gin.H{
+		"message": "success",
+		"delay":   delay,
+	})
+}
